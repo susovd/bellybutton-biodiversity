a76be4cd8de0722050a15af9e315783ef5263b5a
susovd <dhakalsusov@gmail.com>
2020-10-20 19:29:08 +1100

    working codes for graphs

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,31 @@
-//horizontal bar graph
-function bargraph(userInput) {
-  //read the data
+
+//horizontal bar graph
+function bargraph(id_iput) {
+  //read the data
   d3.json("samples.json").then((data_json) => {
-    //get the data of ids, sample values hover text ->labels
-    var sampleinfo = data_json.samples;
-    // filter the data to only get the information associated with the input id
-    var idData = sampleinfo.filter(x => x.id == userInput);
-    //since it is an array get the first array of otu_ids
-    var otu_ids = idData[0].otu_ids;
-    var otu_labels = idData[0].otu_labels;
-    var otu_values = idData[0].sample_values;
-    //format your id to add OTU in the beginning
+    //get the data of ids, sample values hover text ->labels
+    var samplesdata = data_json.samples;
+    // filter the data to only get the information associated with the input id
+    var id_data = samplesdata.filter(x => x.id == id_iput);
+    //since it is an array get the first array of otu_ids
+    var otu_ids = id_data[0].otu_ids;
+    var otu_labels = id_data[0].otu_labels;
+    var otu_values = id_data[0].sample_values;
+    //format your id to add OTU in the beginning
     var yValues = otu_ids.slice(0, 10).map(x => "OTU" + x).reverse()
-    //this where you select your x axis value, y axis value and type of graph
-    var barData = {
-      x: otu_values.slice(0, 10).reverse(),     //top 10 values
-      y: yValues,     // formatted ids
-      text: otu_labels.slice(0, 10).reverse(),     //labels
-      type: "bar", //bar graphs
-      orientation: "h" //horizontal graphs
+    //this where you select your x axis value, y axis value and type of graph
+    var readData = {
+      x: otu_values.slice(0, 10).reverse(),     //top 10 values
+      y: yValues,     // formatted ids
+      text: otu_labels.slice(0, 10).reverse(),     //labels
+      type: "bar", //bar graphs
+      orientation: "h" //horizontal graphs
     };
-    var Layout = {
-      title: "Bacteria found"
+    var setLayout = {
+      title: "Bacteria found"
     };
-    var data = [barData];
-    Plotly.newPlot("bar", data, Layout);
+    var data = [readData];
+    Plotly.newPlot("bar", data, setLayout);
 
     var bubbleData = {
       x: otu_ids,
