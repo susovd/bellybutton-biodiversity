d9e9e31c8387cc02d5a40ba2352101731706beab
susovd <dhakalsusov@gmail.com>
2020-10-20 19:26:20 +1100

    working codes for graphs

--- static/js/app.js
+++ static/js/app.js
@@ -1,32 +1,27 @@
-//horizontal bar graph
+//horizontal bar graph
 function bargraph(userInput) {
-  //read the data
+  //read the data
   d3.json("samples.json").then((data_json) => {
-    //get the data of ids, sample values hover text ->labels
+    //get the data of ids, sample values hover text ->labels
     var sampleinfo = data_json.samples;
-    // filter the data to only get the information associated with the input id
-    var DataId = sampleinfo.filter(x => x.id == userInput);
-    // get the first array of otu_ids
-    var otu_ids = DataId[0].otu_ids;
-    var otu_labels = DataId[0].otu_labels;
-    var otu_values = DataId[0].sample_values;
-    //format id to add OTU in the beginning
+    // filter the data to only get the information associated with the input id
+    var idData = sampleinfo.filter(x => x.id == userInput);
+    //since it is an array get the first array of otu_ids
+    var otu_ids = idData[0].otu_ids;
+    var otu_labels = idData[0].otu_labels;
+    var otu_values = idData[0].sample_values;
+    //format your id to add OTU in the beginning
     var yValues = otu_ids.slice(0, 10).map(x => "OTU" + x).reverse()
-    //create traces and layout for graph
+    //this where you select your x axis value, y axis value and type of graph
     var barData = {
-      //top 10 values
-      x: otu_values.slice(0, 10).reverse(),     
-      // formatted ids
-      y: yValues,
-      //labels     
-      text: otu_labels.slice(0, 10).reverse(),     
-      //bar graphs
-      type: "bar",
-      //horizontal graphs 
-      orientation: "h" 
+      x: otu_values.slice(0, 10).reverse(),     //top 10 values
+      y: yValues,     // formatted ids
+      text: otu_labels.slice(0, 10).reverse(),     //labels
+      type: "bar", //bar graphs
+      orientation: "h" //horizontal graphs
     };
     var Layout = {
-      title: "Bacteria found"
+      title: "Bacteria found"
     };
     var data = [barData];
     Plotly.newPlot("bar", data, Layout);
@@ -40,10 +35,9 @@ function bargraph(userInput) {
         size: otu_values,
         color: otu_ids,
         colorscale: "Earth"
-      };
-    };
-    
-      //bubble graph
+      }
+    }
+
     var bubbleLayout = {
       title: "Bacteria cultures per sample",
       margin: {
@@ -56,16 +50,6 @@ function bargraph(userInput) {
     }
     Plotly.newPlot("bubble", [bubbleData], bubbleLayout)
 
-    // Select the metadata array and for each item append the item ID and adds ID to dropdown
-    var metadata = data_json.metadata; //assign data.metadata to a var to have cleaner code
-    var idMetadata = metadata.filter(x=> (x.id == userInput)); //
-
-    var metadataOutput = idMetadata[0]; //pull first object entry from idMetadata[0] and assign it to a separate variable
-    const panelDisplay = d3.select("#sample-metadata");
-    panelDisplay.html("");  //clear any existing metadata
-    Object.entries(metadataOutut).forEach(([key, value])=> {
-      panelDisplay.append("p").text(`${key.toUpperCase()}: ${value}`);
 
   });
-  bargraph(d3.select("#selDataset")); //calling bargraph function
 };
